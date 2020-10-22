6c78b715a2ab0ce17d436d061f52a495a7481000
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-10-22 11:34:47 +0530

    Update ReadMessageFromFile.h

--- SenderTest1/ReadMessageFromFile.h
+++ SenderTest1/ReadMessageFromFile.h
@@ -2,6 +2,23 @@
 
 #include "util_structures.h"
 
+static MC_STATUS NOEXP_FUNC StreamToMsgObj(int A_msgID,
+	void*      A_CBinformation,
+	int        A_isFirst,
+	int*       A_dataSize,
+	void**     A_dataBuffer,
+	int*       A_isLast);
+SAMP_BOOLEAN isFirstFile(int isFirstFlag, CBinfo* A_cbinfo);
+SAMP_BOOLEAN isLastFile(int eof, int* isLast);
+
+void openEmptyMessage(int* A_msgID);
+void openFile(CBinfo* A_cbinfo, char* A_filename, int* A_msgID);
+void setIOBuffer(CBinfo* A_cbinfo);
+void setBufferLength(CBinfo* A_cbinfo);
+void allocateMemoryToBuffer(CBinfo* A_cbinfo);
+void openEmptyMessage(int* A_msgID);
+void closeFile(CBinfo* A_cbinfo);
+void freeBuffer(CBinfo* A_cbinfo);
 
 
 class ReadMessageFromFile {
