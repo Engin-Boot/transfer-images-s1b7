43f54d44ad8bd2d14329d0a92ece83f870136ee1
amutamil <amuthamizhkavioviyan@gmail.com>
2020-10-27 16:00:46 +0530

    mod

--- DiagonosisStatus/PublishDiagonisedImageName.py
+++ DiagonosisStatus/PublishDiagonisedImageName.py
@@ -1,4 +1,3 @@
-import pycontext  # Ensures paho is in PYTHONPATH
 import paho.mqtt.publish as publish
 msg= input("Enter the Image Name")
 publish.single("ImageName", msg , hostname="mqtt.eclipse.org")
