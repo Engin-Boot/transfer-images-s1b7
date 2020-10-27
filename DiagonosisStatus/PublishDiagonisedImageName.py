129083218d1ea267271fb1fb292c5fb67a95f9f3
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-10-27 19:14:22 +0530

    Update PublishDiagonisedImageName.py

--- DiagonosisStatus/PublishDiagonisedImageName.py
+++ DiagonosisStatus/PublishDiagonisedImageName.py
@@ -1,3 +1,6 @@
 import paho.mqtt.publish as publish
 msg= input("Enter the Image Name")
-publish.single("ImageName", msg , hostname="mqtt.eclipse.org")
+try:
+    publish.single("ImageName", msg , hostname="mqtt.eclipse.org")
+except:
+    print("Failed to Connect")
