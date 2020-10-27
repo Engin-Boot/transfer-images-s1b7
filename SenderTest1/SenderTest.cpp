7a7a301333fb24a365e7d9686f420ab6c7c2e21e
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-10-27 19:06:49 +0530

    Update SenderTest.cpp

--- SenderTest1/SenderTest.cpp
+++ SenderTest1/SenderTest.cpp
@@ -6,46 +6,50 @@
 #include "SenderMainFunctions.h"
 using namespace std;
 
-
-
-
-TEST_CASE("when the character array of -p and port number and its corresponding position and storage option is given then setLocalPortNumber sets"
+TEST_CASE("when the array of -b and port number and its corresponding position and storage option is given then setLocalPortNumber sets"
 "the corresponding port number")
 {
-	char *argv[] = { (char*)"-b",(char*)"676" };
-	int pos = 0;
-	STORAGE_OPTIONS options;
+	char            *argv[] ={ (char*)"-b",(char*)"676" };
+	int              pos = 0;
+	STORAGE_OPTIONS  options;
+
 	setLocalPortNumber(argv, pos, &options);
+
 	REQUIRE(options.ListenPort == 676);
 }
 
-TEST_CASE("when the character array of -n and host name and its corresponding position and storage option is given then setRemoteHostName sets"
+TEST_CASE("when the array of -n and host name and its corresponding position and storage option is given then setRemoteHostName sets"
 	"the corresponding host name to storage options")
 {
-	char *argv[] = { (char*)"-n",(char*)"localhost" };
-	int pos = 0;
-	STORAGE_OPTIONS options;
+	char                *argv[] = { (char*)"-n",(char*)"localhost" };
+	int                  pos = 0;
+	STORAGE_OPTIONS      options;
+
 	setRemoteHostName(argv, pos, &options);
+
 	REQUIRE(strcmp(options.RemoteHostname ,"localhost")==0);
 }
 
-TEST_CASE("when the character array of -p and its port number and its corresponding position and storage option is given then setRemotePortNumber sets"
+TEST_CASE("when the  array of -p and its port number and its corresponding position and storage option is given then setRemotePortNumber sets"
 	"the corresponding port number to storage options")
 {
-	char *argv[] = { (char*)"-p",(char*)"6767" };
-	int pos = 0;
-	STORAGE_OPTIONS options;
+	char               *argv[] = { (char*)"-p",(char*)"6767" };
+	int                pos = 0;
+	STORAGE_OPTIONS    options;
+
 	setRemotePortNumber(argv, pos, &options);
+
 	REQUIRE(options.RemotePort == 6767);
 }
 
-TEST_CASE("when the command line argument and the argument count is given then the TestCmdLine function returns TRUE SAMP_BOOLEAN if all parameters are already set")
+TEST_CASE("when the command line argument and the argument count is given then the TestCmdLine function returns SAMP_TRUE indicating that all parameters are set")
 {
-	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" }; 
+	char*                  argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" }; 
+	int                    argc = 6;
+	STORAGE_OPTIONS        options;
 
-	int argc = 6;
-	STORAGE_OPTIONS options;
 	TestCmdLine(argc, argv, &options);
+
 	REQUIRE(TestCmdLine(argc, argv, &options) == SAMP_TRUE);
 }
 
@@ -74,7 +78,7 @@ TEST_CASE("AddFiletoList when called on non-empty list then returns true on succ
 }
 
 
-TEST_CASE("Get number of nodes in the linked list when called then returns number of nodes")
+TEST_CASE("When the node head is given then GetNumNodes returns the number of nodes")
 {
 	auto node1 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
 	auto node2 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
@@ -86,7 +90,7 @@ TEST_CASE("Get number of nodes in the linked list when called then returns numbe
 	REQUIRE(image.GetNumNodes(node1) == 2);
 }
 
-TEST_CASE("Get number of nodes in the linked list when called with no nodes then returns 0 number of nodes")
+TEST_CASE("When empty list node head is given then GetNumNodes returns 0 ")
 {
 	auto node1 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
 	auto node2 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
@@ -118,7 +122,7 @@ TEST_CASE("Get number of outstanding requests when called then returns number of
 	REQUIRE(image.GetNumOutstandingRequests(node1) == 2);
 }
 
-TEST_CASE("FreeList when called frees the linked list passed")
+TEST_CASE("When the list head is given then FreeList frees the linked list")
 {
 	auto node1 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
 	auto node2 = static_cast<InstanceNode*>(malloc(sizeof(InstanceNode)));
@@ -133,102 +137,114 @@ TEST_CASE("FreeList when called frees the linked list passed")
 	REQUIRE(node1 == nullptr);
 }
 
-TEST_CASE("When the instanceList pointer address,storage options and imageList if given then the addImageTOList function"
-"adds the image to the list")
+
+TEST_CASE("When the instanceList pointer address,storage options with two images  and imageList is given then the addImageTOList function"
+"adds the two images to the imagelist")
 {
-	STORAGE_OPTIONS options;
-	InstanceNode *instanceList = NULL, *node = NULL;
-	ImageList* imgList = new ImageList;
+	STORAGE_OPTIONS                   options;
+	InstanceNode                      *instanceList = NULL, *node = NULL;
+	ImageList                         *imgList = new ImageList;
+
+
 	options.StartImage =0;
 	options.StopImage = 1;
 	addImagesToList(&options, imgList, &instanceList);
+
 	int totalImages = 2;
 
 	REQUIRE(imgList->GetNumNodes(instanceList)==totalImages);
 
 }
-TEST_CASE("when the address of  applicationId ,AssociationId and storage Options is given then the"
-	"openAssociation Function open the association sets the applicationID and AssociationId")
-{
-	int  applicationID = -1, associationID = -1;
-	STORAGE_OPTIONS options;
-	MC_STATUS mcStatus;
-	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
 
-	int argc = 6;
-	InstanceNode *instanceList = NULL, *node = NULL;
-	ImageList* imgList = new ImageList;
+TEST_CASE("when the address of  applicationId ,AssociationId and storage Options is given with no library initialization then the"
+	"openAssociation Function does not open the association")
+{
+	int                         applicationID = -1, associationID = -1;
+	STORAGE_OPTIONS             options;
+	MC_STATUS                   mcStatus;
+	int                         argc = 6;
+	InstanceNode                *instanceList = NULL, *node = NULL;
+	ImageList                   *imgList = new ImageList;
+	char* argv[] =              { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
+	
+	
 	TestCmdLine(argc, argv, &options);
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
-	mcStatus = MC_Register_Application(&applicationID, options.LocalAE);
-	addImagesToList(&options, imgList, &instanceList);
+
+	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	openAssociation(&applicationID, &associationID, &options);
 	mcStatus = MC_Get_Association_Info(associationID, &options.asscInfo);
+
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
 	closeAssociation(&applicationID, &associationID);
 
 }
-TEST_CASE("when the options,applicationID and instanceList with no library initialization is given then read function return SAMP_FALSE")
-{
-	SAMP_BOOLEAN            sampBool;
-	ReadImage* reader = new ReadImage;
-	STORAGE_OPTIONS options;
-	int applicationID = -1;
-	InstanceNode   *node = NULL;
-	MC_STATUS mcStatus;
-	
-	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
 
+TEST_CASE("when the storage options,applicationID and instanceList with no library initialization is given"
+	"then read function return SAMP_FALSE")
+{
+	SAMP_BOOLEAN                    sampBool;
+	STORAGE_OPTIONS                 options;
+	int                             applicationID = -1;
+	InstanceNode                    *node = NULL;
+	MC_STATUS                       mcStatus;
+	InstanceNode                    *instanceList = NULL;
+	ImageList                       *imgList = new ImageList;
+	ReadImage                       *reader = new ReadImage;
+
+	char*argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
 	int argc = 6;
-	InstanceNode *instanceList = NULL;
-	ImageList* imgList = new ImageList;
+	
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	TestCmdLine(argc, argv, &options);
 	addImagesToList(&options, imgList, &instanceList);
 	node = instanceList;
 	sampBool = reader->read(&options, applicationID, node);
 	REQUIRE(sampBool == SAMP_FALSE);
 }
-TEST_CASE("when the valid storage options,associationID,and node then send fucntion return SAMP_TRUE")
+
+TEST_CASE("when the storage options,associationID,and node is given  then send fucntion return SAMP_TRUE")
 {
-	STORAGE_OPTIONS options;
-	int associationID = -1;
-	InstanceNode   *node = NULL;
-	MC_STATUS mcStatus;
+	SAMP_BOOLEAN                  sampBool;
+	STORAGE_OPTIONS               options;
+	int                           associationID = -1;
+	InstanceNode                  *node = NULL;
+	MC_STATUS                     mcStatus;
+	InstanceNode                  *instanceList = NULL;
+	ImageList                     *imgList = new ImageList;
+	SendImage                     *sender = new SendImage;
 
 	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
-
 	int argc = 6;
-	InstanceNode *instanceList = NULL;
-	ImageList* imgList = new ImageList;
+	
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	TestCmdLine(argc, argv, &options);
 	addImagesToList(&options, imgList, &instanceList);
 	node = instanceList;
-
-	SAMP_BOOLEAN sampBool;
-	SendImage *sender = new SendImage;
-	
 	sampBool = sender->send(&options, associationID, node);
 	REQUIRE(sampBool == SAMP_TRUE);
 }
 
 TEST_CASE("When the imageSent as SAMP_TRUE are given then setnodeResponseParamsOnSucces set the imagesSent count to 1")
 {
-	STORAGE_OPTIONS options;
-	int associationID = -1;
-	InstanceNode   *node = NULL;
-	MC_STATUS mcStatus;
+	STORAGE_OPTIONS            options;
+	int                        associationID = -1;
+	InstanceNode               *node = NULL;
+	MC_STATUS                  mcStatus;
+	InstanceNode               *instanceList = NULL;
+	ImageList                  *imgList = new ImageList;
 
 	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
-
 	int argc = 6;
-	InstanceNode *instanceList = NULL;
-	ImageList* imgList = new ImageList;
+	
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	TestCmdLine(argc, argv, &options);
 	addImagesToList(&options, imgList, &instanceList);
 	node = instanceList;
@@ -240,18 +256,19 @@ TEST_CASE("When the imageSent as SAMP_TRUE are given then setnodeResponseParamsO
 
 TEST_CASE("When the imageSent as SAMP_FALSE is  given then setnodeResponseParamsOnSucces sets the ResponseReceived and FailedResponse as SAMP_TRUE")
 {
-	STORAGE_OPTIONS options;
-	int associationID = -1;
-	InstanceNode   *node = NULL;
-	MC_STATUS mcStatus;
+	STORAGE_OPTIONS               options;
+	int                           associationID = -1;
+	InstanceNode                  *node = NULL;
+	MC_STATUS                     mcStatus;
+	InstanceNode                  *instanceList = NULL;
+	ImageList                     *imgList = new ImageList;
 
 	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
-
 	int argc = 6;
-	InstanceNode *instanceList = NULL;
-	ImageList* imgList = new ImageList;
+	
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	TestCmdLine(argc, argv, &options);
 	addImagesToList(&options, imgList, &instanceList);
 	node = instanceList;
@@ -262,28 +279,26 @@ TEST_CASE("When the imageSent as SAMP_FALSE is  given then setnodeResponseParams
 	REQUIRE(node->responseReceived == SAMP_TRUE);
 }
 
-TEST_CASE("When the Storage options,associationId and instaceList with no successfull libraray initialization then"
+TEST_CASE("When the Storage options,associationId and instaceList is given with no successfull libraray initialization then"
 "the readResponseMessages return SAMP_FALSE")
 {
-	SAMP_BOOLEAN sampBool;
-	STORAGE_OPTIONS options;
-	int associationID = -1;
-	InstanceNode   *node = NULL;
-	MC_STATUS mcStatus;
+	SAMP_BOOLEAN                         sampBool;
+	STORAGE_OPTIONS                      options;
+	int                                  associationID = -1;
+	InstanceNode                         *node = NULL;
+	MC_STATUS                            mcStatus;
+	InstanceNode                         *instanceList = NULL;
+	ImageList                            *imgList = new ImageList;
 
 	char* argv[] = { (char*)"sender.exe",(char*)"MERGE_STORE_SCP",(char*)"0.img",(char*)"1.img",(char*)"-p",(char*)"6767" };
-
 	int argc = 6;
-	InstanceNode *instanceList = NULL;
-	ImageList* imgList = new ImageList;
+	
 	mcStatus = MC_Library_Initialization(NULL, NULL, NULL);
 	REQUIRE(mcStatus != MC_NORMAL_COMPLETION);
+
 	TestCmdLine(argc, argv, &options);
 	addImagesToList(&options, imgList, &instanceList);
 	node = instanceList;
-	
 	sampBool = ReadResponseMessages(&options, associationID, 1, &instanceList, NULL);
 	REQUIRE(sampBool == SAMP_FALSE);
 }
-
-
